0b9064fbf2d568c32e08e0f6e8600fca9ffab597
Jacek Tomaszewski <jacek.tomek@gmail.com>
2013-12-08 22:08:57 +0100

    Add setup file with requirements.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name='Supermarket Metodyczny',
+    version='0.1',
+    description='Strona na której instruktorzy będą mogli zaczerpnąć wiedzę o pomysłach na realizację form dostosowanych do warunków harcerza',
+    author="Szymon Kuliński",
+    author_email="szymon.kulinski@zhr.pl",
+    install_requires=(
+        'django>=1.6,<1.7',
+        'south',
+        'django-admin-tools',
+        'django-generic-positions',
+    ),
+    packages=find_packages(),
+    include_package_data=True,
+)
