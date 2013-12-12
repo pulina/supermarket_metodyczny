82fdd49be01730be14aee250d5ad963e631c4be6
Jacek Tomaszewski <jacek.tomek@gmail.com>
2013-12-12 21:23:41 +0100

    make sort order readonly in admin.

--- base/admin.py
+++ base/admin.py
@@ -20,6 +20,7 @@ admin.site.register(Funkcja)
 class okresAdmin(OrderableAdmin, MyAdmin):
     list_display = ('sort_order_display', '__str__')
     list_display_links = ('__str__',)
+    readonly_fields = ('sort_order',)
 
     class Media:
         js = ('http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.js',)
