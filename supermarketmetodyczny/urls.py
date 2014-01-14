7f6f55005c49383ea1c46faf0886c07c9ce4ae3c
Supermarket Metodyczny <supermarket@users.zhr.pl>
2014-01-14 22:16:27 +0100

    Add cache to jsreverse view.

--- supermarketmetodyczny/urls.py
+++ supermarketmetodyczny/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.decorators.cache import cache_page
+
+from django_js_reverse.views import urls_js
+
 import autoload
 
 
@@ -8,6 +12,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin_tools/', include('admin_tools.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^jsreverse/$', 'django_js_reverse.views.urls_js', name='js_reverse'),
+    url(r'^jsreverse/$', cache_page(3600)(urls_js), name='js_reverse'),
     url(r'', include('base.urls')),
 )
